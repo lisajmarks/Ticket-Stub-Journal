56f6af8ab6d95b95cef04e115e97684b8c0aa6d7
Lisa Yang <lisajmarks@protonmail.com>
2021-04-30 03:44:17 +0000

    update model.py and test user and events tables

--- website/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask 
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'k23lkjfwa-9adkljfg' #secures cookies and sessions data
-
-    return app 
--- website/model.py
+++ website/model.py
@@ -1,6 +1,9 @@
 """Models for movie  Ticket Stub Journal."""
 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 db = SQLAlchemy()
 
@@ -10,12 +13,13 @@ class User(db.Model):
     __tablename__ = "users"
     #TODO: Add nullables#
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    name = db.Column(db.string)
-    password = db.Column(db.String)
+    profile_pic = db.Column(db.Integer, db.ForeignKey('pictures.pic_id'))
+    email = db.Column(db.Text, unique=True)
+    name = db.Column(db.Text)
+    password = db.Column(db.Text)
     join_date = db.Column(db.DateTime)
     bio = db.Column(db.Text)
-    city = db.Column(db.String)
+    city = db.Column(db.Text)
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
@@ -28,8 +32,8 @@ class Event(db.Model):
     event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     #user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('venues.venue_id'), nullable=False)
-    pic_id = db.Column(db.Integer, db.ForeginKey('pictures.pic_id'))
-    event_name = db.Column(db.String)
+    # pic_id = db.Column(db.Integer, db.ForeginKey('pictures.pic_id'))
+    event_name = db.Column(db.Text)
     date = db.Column(db.DateTime)
 
 class Venue(db.Model):
@@ -41,10 +45,10 @@ class Venue(db.Model):
     #event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
-    street_address = db.Column(db.String)
-    city = db.Column(db.String)
-    state = db.Column(db.String)
-    description = db.Column(db.String)
+    street_address = db.Column(db.Text)
+    city = db.Column(db.Text)
+    state = db.Column(db.Text)
+    description = db.Column(db.Text)
     
     def __repr__(self):
         return f'<location venue_id={self.loc_id} name={self.name}'
@@ -57,30 +61,67 @@ class Memory(db.Model):
     memory_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'), nullable=False)
-    fav_song = db.Column(db.String)
+    fav_song = db.Column(db.Text)
     memory = db.Column(db.Text)
-    squad = db.Column(db.String)
+    squad = db.Column(db.Text)
 
 class UserEvent(db.Model):
     """A User's Event"""
 
     __tablename__= "user_event"
 
-    user_event_id = db.Column(db.integer, autoincrement=True, primary_key=True)
+    user_event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'), nullable=False)
+    pic_id = db.Column(db.Integer, db.ForeignKey('pictures.pic_id'))
 
     user_event = db.relationship('User', backref='events')
     events_user = db.relationship('Event', backref='users')
 
 class Picture(db.Model):
     """A Picture"""
-    #install URL type furl? https://github.com/gruns/furl
+    #TODO: install URL type furl? https://github.com/gruns/furl
     __tablename__ = "pictures"
 
-    pic_id = db.Column(db.integer, autoincrement=True, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'),nullablle=False)
-    loc = db.Column(URLType)
+    pic_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    # event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'),nullablle=False)
+    loc = db.Column(db.Text)
+
+def generate_user_test_data():
+    test_pic= Picture(pic_id='10', loc='test')
+    db.session.add(test_pic)
+    db.session.commit()
+    test_user= User(profile_pic='10', email='test@test.com', name='test', password='test', join_date='1000-10-10', bio='test', city='test')
+    db.session.add(test_user)
+    db.session.commit()
+
+def generate_event_test_data():
+    venue_id= Venue(venue_id='1')
+    db.session.add(venue_id)
+    db.session.commit()
+    test_event= Event(event_id='1',venue_id='1', event_name='OSL', date='1001-11-11')
+    db.session.add(test_event)
+    db.session.commit()
+
+# def create_user(email,password):
+#     user = User(email = email, password = password)
+#     db.session.add(user)
+#     db.session.commit()
 
 
+def connect_to_db(flask_app, db_uri='postgresql:///tsj', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+if __name__ == '__main__':
+
+    from server import app
+
+    connect_to_db(app)

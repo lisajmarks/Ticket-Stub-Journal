c2816bfb5272cd5d45af8e266f6e29d2e85f9474
Lisa Yang <lisajmarks@protonmail.com>
2021-05-10 23:33:46 +0000

    update memory table

---
+++ crud.py
@@ -0,0 +1,112 @@
+"""CRUD operations."""
+
+from jinja2.runtime import Undefined
+from model import db, User, Event, Venue, Memory, Picture, connect_to_db
+
+
+# Functions start here!
+
+def clear_tables():
+    db.create_all()
+    Memory.query.delete()
+    User.query.delete()
+    Event.query.delete()
+    Venue.query.delete()
+    UserEvent.query.delete()
+    Picture.query.delete()
+    db.session.commit()
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+# def create_venue(latitude, longitude, street_address, city, state):
+#     """Create and return a venue"""
+    
+#     venue = Venue(latitude=latitude, longitude=longitude, street_address=street_address, city=city, state=state)
+#     db.session.add(venue)
+#     db.session.commit()
+
+#     return venue
+
+def create_venue(name, street_address='', city='', state='', latitude=0, longitude=0):
+    """Create and return a venue"""
+    
+    venue = Venue(name=name, latitude=latitude, longitude=longitude, street_address=street_address, city=city, state=state)
+    db.session.add(venue)
+    db.session.commit()
+
+    return venue
+
+
+
+def create_event(venue_id, event_name, date):
+    """Create and return a new event"""
+    event = Event(venue_id=venue_id, event_name=event_name, date=date)
+
+    db.session.add(event)
+    db.session.commit()
+
+    return event 
+
+
+
+def create_memory(user_id, event_id, fav_song, memory, squad, pic_id=None):
+    """Create and return a memory"""
+
+    # user = User(user_id=user_id)
+    # db.session.add(user)
+    # db.session .commit()
+
+    # event = Event(event_id=event_id)
+    # db.session.add(event)
+    # db.session.commit()
+
+    memory = Memory(user_id=user_id, event_id=event_id, fav_song=fav_song, memory=memory, squad=squad, pic_id=pic_id)
+    db.session.add(memory)
+    db.session.commit()
+
+    return memory 
+
+def create_picture(pic_id, loc):
+    pic = Picture(pic_id=pic_id, loc=loc)
+    db.session.add(pic)
+    db.session.commit()
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+
+    return User.query.get(user_id)
+
+def create_user_event(user_id, event_id, pic_id=""):
+    user_event = UserEvent(user_id=user_id, event_id=event_id, pic_id=pic_id)
+    db.session.add(user_event)
+    db.session.commit()
+
+    return user_event
+
+def get_events_by_userid(user_id): 
+    #TODO: finish this function 
+    """Return a users event info by user id"""
+    user = User.query.get(user_id)
+    print(user.query.memories.get(fav_song))
+    print(user.query.memories.get(memory))
+
+
+#TODO sqlalchemy join - results list (list of dictionaries)
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
---
+++ server.py
@@ -0,0 +1,126 @@
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import connect_to_db
+import crud
+from jinja2 import StrictUndefined
+# from website import create_app
+
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'k23lkjfwa-9adkljfg' #secures cookies and sessions data
+
+    return app 
+app = create_app()
+
+
+@app.route('/')
+def homepage():
+    """View homepage"""
+    return render_template('homepage.html')
+
+@app.route('/profile')
+def profile():
+    """View user profile"""
+    return render_template('profile.html')
+
+@app.route('/map')
+def map():
+    """View user event map"""
+    return render_template('map.html')
+
+#TODO: fix this route so it displays userevents
+@app.route('/events')
+def events():
+    """View user events"""
+    events = crud.get_events_by_id(name, event_name, fav_song, memories, squad)
+    return render_template('events.html', events = events)
+
+@app.route('/shows')
+def shows():
+    """View user shows"""
+    return render_template('shows.html')
+
+@app.route('/addshow')
+def add_show():
+    """Add show info"""
+    return render_template('addshow.html')
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Create a new user""" 
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_email(email)
+    if user: 
+        flash("Oops - Cannot create an account with that email. Try again.")
+    else: 
+        crud.create_user(email, password)
+        flash("Account created! Log in and join the party!")
+
+    return redirect('/')
+
+@app.route("/users/<user_id>")
+def show_user(user_id): 
+    """Show details on a particular view."""
+
+    user = crud.get_user_by_id(user_id)
+    return render_template ("user_details.html", user=user) 
+
+@app.route("/login", methods=["POST"])
+def process_login(): 
+    """Process User login.""" 
+    email = request.form.get("login_email")
+    password = request.form.get("login_password")
+
+    user = crud.get_user_by_email(email)
+    if not user or user.password != password: 
+        flash("Whoa - your email or password is incorrect!")
+        print("{user.email}")
+        print("{user.password}")
+        print(email)
+        print(password)
+    else: 
+        #log in user by storing the user's email in session 
+        session["user_email"] = user.email 
+        session["user_id"] = user.user_id
+        flash(f"Welcome back, {user.email}!")
+    
+    return redirect("/")
+
+@app.route("/events/create", methods=["POST"])
+def create_event_backend():
+    """Create User Event"""
+    event_name = request.form.get("event_name")
+    date = request.form.get("date")
+    venue_name = request.form.get("venue")
+    fav_song = request.form.get("fav_song")
+    memories = request.form.get("memory")
+    squad = request.form.get("squad")
+    user_id = session["user_id"]
+
+    v = crud.create_venue(venue_name)
+    e = crud.create_event(v.venue_id, event_name, date)
+    m = crud.create_memory(user_id, e.event_id, fav_song, memories, squad)
+    user_event = crud.create_user_event(user_id, e.event_id)
+
+    return redirect("/events")
+
+@app.route("/events/create")
+def create_event_page():
+    """Display Create User Event Page""" 
+
+    return render_template("/create-event.html")
+
+if __name__ == '__main__': 
+#only if we run this file we will excute app.run(debug=True). 
+#Only run web server if we run this file. 
+#Turn off when running in production
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)

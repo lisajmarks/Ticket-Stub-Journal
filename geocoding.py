61fea10ebf9f8daf30b7f07dd49c6c9301f9e560
Lisa Yang <lisajmarks@protonmail.com>
2021-06-10 22:44:55 +0000

    rotate key

--- geocoding.py
+++ geocoding.py
@@ -1,6 +1,7 @@
 import requests
 
-key = "AIzaSyDwx11RUEtpBZVo1XBAeSPaXIp9CN6S3AQ"
+# key = "AIzaSyDwx11RUEtpBZVo1XBAeSPaXIp9CN6S3AQ"
+key = "G_MAPS_API_SECRET"
 url = "https://maps.googleapis.com/maps/api/geocode/json"
 
 def get_coordinates(address):
